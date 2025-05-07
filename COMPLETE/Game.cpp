ac074589a4044c67d583571ee5b4b11a7c6182b3
ashtontheanalyst <ashtonrwsmith.co@gmail.com>
2025-05-06 19:30:56 -0500

    First stable and finished version of the game!

---
+++ COMPLETE/Game.cpp
@@ -0,0 +1,299 @@
+// This file implements the logic for the Game class
+// this->enemy is like writing Game::enemy, the 'this' is referencing Game!!
+
+#include "Game.h" // Brings in the declarations of the Game class
+
+// PRIVATE FUNCTIONS --------------------------------------------------------------------------
+void Game::initVariables() {
+    // Initializes the window pointer to hold a value of nullptr
+    this->window = nullptr;
+
+    // Game logic
+    this->endGame = false;
+    this->points = 0;
+    this->health = 100; // max starting health
+    this->enemySpawnTimerMax = 25.f; // Making this smaller means enemies spawn faster, larger number is the opp.
+    this->enemySpawnTimer = this->enemySpawnTimerMax; // timer starts at its max value
+    this->maxEnemies = 40;
+    this->mouseHeld = false; // not currently holding down on the mouse, looking for indiv. clicks
+}
+
+
+void Game::initWindow() {
+    // Setting the screen size
+    this->videoMode.height = 600;
+    this->videoMode.width = 800;
+    
+    // Creates a new SFML window using the pointer this->window
+    this->window = new sf::RenderWindow(this->videoMode, "Game 1", sf::Style::Titlebar | sf::Style::Close);
+
+    this->window->setFramerateLimit(60); // Set the refresh rate at 60Hz
+}
+
+
+// Import the ttf file for our text
+void Game::initFonts() {
+     // make sure the path is right and accessible
+    if (this->font.loadFromFile("/home/dementus/code/cpp/cpp-SFML-tutorials/RobotoCondensed-VariableFont_wght.ttf")) {
+        std::cout << "ERROR in INIT: Failed to load font..." << '\n';
+    }
+}
+
+
+// Initialize our text and what it default is
+void Game::initText() {
+    this->uiText.setFont(this->font); // sets the font for our text object
+    this->uiText.setCharacterSize(35); // font size
+    this->uiText.setFillColor(sf::Color::White);
+    this->uiText.setString("ERROR: Not Set"); // Default, will be changed in game
+}
+
+
+void Game::initEnemies() {
+    this->enemy.setPosition(sf::Vector2f(20.f, 20.f)); // Sets the def. position of the enemy
+    // For positioning, the x is from left (0) to right (max), the y is from top (0) to bottom (max)
+    
+    // the setSize function makes us use this Vector2f one, then we put in const sizes (h/w pixels)
+    this->enemy.setSize(sf::Vector2f(100.f, 100.f)); // the .f signifies it's a float value
+    this->enemy.setScale(sf::Vector2f(0.5f, 0.5f)); // cuts the size in half since this is a scaling function
+
+    // The rest of these are filling out what the enemy is, what it looks like, etc.
+    this->enemy.setFillColor(sf::Color::Yellow);
+}
+
+
+// Constructor, this will run once Game game; is called in main.cpp
+Game::Game() {
+    // Calls these private functions to initialize the window and variables
+    this->initVariables();
+    this->initWindow();
+    this->initFonts();
+    this->initText();
+    this->initEnemies();
+}
+
+
+// Destructor, runs at the end of main
+Game::~Game() {
+    delete this->window; // frees the memory we used to create the new window
+}
+
+
+
+
+// PUBLIC FUNCTIONS ---------------------------------------------------------------------------
+// Accessor, main.cpp is asking if the window is still open or not
+const bool Game::running() const {
+    return this->window->isOpen();
+}
+
+
+// Returns if the game has ended or not
+const bool Game::getEndGame() const {
+    return this-> endGame;
+}
+
+
+// Spawns our enemies and sets random types, colors, positions, and adds them to the enemies vector
+void Game::spawnEnemy() {
+    this->enemy.setPosition(
+        // boundary values between 0 and the window sizing, static cast to int's
+        // pick a random value between those int's and static cast that value into a float
+        // the y value is just the top of the screen, 0.f
+        static_cast<float> (rand() % static_cast<int>(this->window->getSize().x - this->enemy.getSize().x)),
+        0.f
+    );
+
+    // Randomize the enemy type
+    int type = rand() % 4; // from 0 to 4, so 5 total types
+
+    switch(type) {
+        case(0):
+            // harest enemy
+            this->enemy.setSize(sf::Vector2f(30.f, 30.f));
+            this->enemy.setFillColor(sf::Color::Red);
+            break;
+        case(1):
+            this->enemy.setSize(sf::Vector2f(70.f, 70.f));
+            this->enemy.setFillColor(sf::Color::Magenta);
+            break;
+        case(2):
+            this->enemy.setSize(sf::Vector2f(100.f, 100.f));
+            this->enemy.setFillColor(sf::Color::Green);
+            break;
+        case(3):
+            this->enemy.setSize(sf::Vector2f(130.f, 130.f));
+            this->enemy.setFillColor(sf::Color::Blue);
+            break;
+        case(4):
+            // easiest
+            this->enemy.setSize(sf::Vector2f(200.f, 200.f));
+            this->enemy.setFillColor(sf::Color::White);
+            break;
+        default:
+            this->enemy.setSize(sf::Vector2f(100.f, 100.f));
+            this->enemy.setFillColor(sf::Color::Yellow);
+            break;
+    }
+    
+    this->enemies.push_back(this->enemy); // adds the just made enemy to the enemies vector
+}
+
+
+// Get's all the pending events
+// Event polling, SFMLs event system allows us to react to things like key inputs, the mouse, etc.
+void Game::pollEvents() {
+    while(this->window->pollEvent(this->ev)) {
+        switch (this->ev.type) {
+            // If the close button is pressed in the window, close it
+            case sf::Event::Closed:
+                this->window->close();
+                break;
+            
+            // If the 'ESC' key is pressed, do the same thing
+            case sf::Event::KeyPressed:
+                if (this->ev.key.code == sf::Keyboard::Escape) {
+                    this->window->close();
+                }
+            break;
+        }
+    }
+}
+
+
+// On screen text
+void Game::updateText() {
+    std::stringstream ss; // initialize a string stream
+
+    // this formatting is super nice, follows what it'll look like on screen
+    ss << "Points: " << this->points << '\n' 
+       << "Health: " << this->health;
+
+    this->uiText.setString(ss.str()); // set the string as the stream in string format
+}
+
+
+// Updates and stores the mouse positions within the game window
+void Game::updateMousePositions() {
+    this->mousePosWindow = sf::Mouse::getPosition(*this->window);
+    this->mousePosView = this->window->mapPixelToCoords(this->mousePosWindow); // window coords casted to view in floats
+}
+
+
+// Updates the enemy spawn timer and spawns enemies when the on-screen amount is smaller than the max
+void Game::updateEnemies() {
+    // if we have less enemies than the max amount...
+    if (this->enemies.size() < this->maxEnemies) {
+        if(this->enemySpawnTimer >= this->enemySpawnTimerMax) {
+            this->spawnEnemy(); // spawn an enemy
+            this->enemySpawnTimer = 0.f; // this resets the timer
+        }
+        else {
+            this->enemySpawnTimer += 1.f; // or increase it if it's less than the max
+        }
+    }
+
+    // Move and update the enemies, check if they're under screen
+    for (int i = 0; i < this->enemies.size(); i++) {
+        this->enemies[i].move(0.f, 7.f); // moving them downward at a rate of 5 pixels/second
+
+        // if the top pixel of the enemy falls below the screen, delete
+        if (this->enemies[i].getPosition().y > this->window->getSize().y) {
+            this->enemies.erase(this->enemies.begin() + i); // delete the enemy at it's potion in the vector
+            this->health -= 10; // every time you miss an enemy subtract 10 from the max health of 100
+        }
+    }
+
+    // Kill the enemy
+    // Check if the enemy has been clicked, if the left mouse button returns true from isButtonPressed()...
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+        
+        // making sure the mouse isn't being held, has to be an indiv. click
+        if (this->mouseHeld == false) {
+            
+            this->mouseHeld = true; // saying there's a click for ONLY this iteration
+            bool deleted = false; // contitonal for killing the enemy
+
+            for (int i = 0; i < this->enemies.size() && deleted == false; i++) {
+                
+                // getGlobalBounds gets the coordinates of the enemy, where is it and how large ish
+                // contains will then check our vector to see if the mouse is inside the enemy box
+                if (this->enemies[i].getGlobalBounds().contains(this->mousePosView)) {
+                    
+                    // Conditional amount of points based on enemy type (color)
+                    if (this->enemies[i].getFillColor() == sf::Color::Red) {
+                        this->points += 100; // hardest enemy
+                    }
+                    else if (this->enemies[i].getFillColor() == sf::Color::Magenta) {
+                        this->points += 70;
+                    }
+                    else if (this->enemies[i].getFillColor() == sf::Color::Green) {
+                        this->points += 50;
+                    }
+                    else if (this->enemies[i].getFillColor() == sf::Color::Blue) {
+                        this->points += 20;
+                    }
+                    else if (this->enemies[i].getFillColor() == sf::Color::White) {
+                        this->points += 5;
+                    }
+
+                    
+                    // deletes the enemy
+                    deleted = true;
+                    this->enemies.erase(this->enemies.begin() + i);
+
+                    this->points += 10; // 10 points per enemy
+                }
+            }
+        }
+    }
+    else {
+        this->mouseHeld = false;
+    }
+}
+
+
+// Updates the game logic every frame. Could be player movement, an attack, screen explosion, etc.
+void Game::update() {
+    this->pollEvents(); // looking for inputs
+    
+    // run the game until it sees an end, triggers it to true
+    if (this->endGame == false) {
+        this->updateMousePositions(); // capture mouse movement data
+        this->updateText();           // get the text for the backend
+        this->updateEnemies();        // get enemy positions and backend
+    }
+
+    if (this->health < 10) {
+        this->endGame = true; // end the game if we run out of health, stops updating the game
+    }
+}
+
+
+// Puts the text on the screen, the RenderTarget is a param. for a standard window to draw or put stuff on
+void Game::renderText(sf::RenderTarget &target) {
+    target.draw(this->uiText);
+}
+
+
+void Game::renderEnemies(sf::RenderTarget &target) {
+    // Rendering all the enemies
+    for (int i = 0; i < this->enemies.size(); i++) {
+        target.draw(this->enemies[i]);
+    }
+}
+
+
+// Putting out all the pizels on the screen
+void Game::render() {
+    /*
+    - Clear the old frame
+    - Render the objects
+    - Display frame in the window
+    */
+
+    this->window->clear();              // clears the screen to black between frames
+    this->renderEnemies(*this->window); // draw the enemies on the screen
+    this->renderText(*this->window);    // put the text on screen
+    this->window->display();
+}
--- README.txt
+++ README.txt
@@ -1,9 +1,34 @@
-Time to learn SFML and make some games
-
-I'll be following this tutorial:
+OVERVIEW:
+This game is based closely on the first game in the tutorial below:
 https://www.youtube.com/watch?v=2FNP3Jce-fc&list=PL6xSOsbVA1eb_QqMTTcql_3PdOiE928up&index=2
 
-Current:
-Video - 9/47
-Time - 8:40
-Concept - Finished implementing the one click rule, so no dragging/spray and pray
+
+GAME DESCRIPTION:
+The game itself is a clicking game, there are 5 different types of enemy blocks falling from
+the ceiling. The smallest block gives you the most points while the largest gives you the
+smallest amount. Failure to kill the enemy will result in health being taken away, missing 10
+enemies will result in a FATALITY!!!...
+
+
+NOTES:
+As of now the game is complete and working. If you clone the 'COMPLETE' repository
+it should have the main.cpp, Game.cpp, and Game.h files which when compiled will run the game.
+To compile on your own, you will need to have all necessary sfml libraries downloaded and C++.
+The output file 'game' is a precompiled file that should be executable. The only other file
+is a .ttf which is a font file for in game text.
+
+
+FILES IN DEPTH:
+main.cpp
+This file holds the main game loop for run Time.
+
+Game.h
+This file is the class definition. There is not much more than simple defining and order here.
+
+Game.cpp
+This defines the var.'s/functions. The private game objects include variables and functions,
+most of these are used for initializing the environment, enemies, and etc. The public objects
+include most of the game functonality, backend upfating, and rendering to the screen. This
+is the most important file for understanding how this game works. Use Game.h to help if you
+get lost, all var.'s and functions on the .h file are in the same order on the .cpp, it's 
+like looking at a snapshot.
